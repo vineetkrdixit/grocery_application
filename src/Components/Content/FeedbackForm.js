6455a43d59e485317b4a46a4f6ae4f3645591d84
vineetkrdixit <vineetdixit.vns@gmail.com>
2022-05-21 21:26:31 +0530

    WIP: done with Review Stars

--- src/Components/Content/FeedbackForm.js
+++ src/Components/Content/FeedbackForm.js
@@ -7,36 +7,36 @@ export default function FeedbackForm() {
       <div className="container w-50">
         <h3 className="mb-5 mt-3">Feedback</h3>
         <form>
-          <div class="mb-3">
-            <label for="exampleInputName" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputName" className="form-label">
               Name :
             </label>
-            <input type="text" class="form-control" id="exampleInputName" />
+            <input type="text" className="form-control" id="exampleInputName" />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
               Email :
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
             />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputFeedback" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputFeedback" className="form-label">
               Feedback :
             </label>
             <textarea
               type="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputFeedback"
             ></textarea>
           </div>
           <ReviewStars />
 
-          <button type="submit" class="btn btn-primary mt-5 mb-3">
+          <button type="submit" className="btn btn-primary mt-5 mb-3">
             Submit
           </button>
         </form>
--- src/Components/Content/ReviewStars.js
+++ src/Components/Content/ReviewStars.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ReviewStars() {
+  const [star, setStar] = useState(0);
   const handelClick = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    // console.log(e.target.getAttribute("value"));
+    setStar(e.target.getAttribute("value"));
   };
   return (
     <div>
@@ -10,7 +13,12 @@ export default function ReviewStars() {
       <span className="">
         {[...Array(5)].map((val, index) => {
           return (
-            <i class="fa fa-star-o" value={index} onClick={handelClick}>
+            <i
+              className={index < star ? "fa fa-star" : "fa fa-star-o"}
+              value={index + 1}
+              onClick={handelClick}
+              key={index}
+            >
               &nbsp;
             </i>
           );
